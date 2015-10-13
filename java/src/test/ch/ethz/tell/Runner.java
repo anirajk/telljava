9c8b65c3828f59d89e933bd0407af03a4349f3ee
Renato Marroquin <renatoj.marroquin@gmail.com>
2015-10-13 18:00:15 +0200

    Fixing record alignment

--- java/src/test/ch/ethz/tell/Runner.java
+++ java/src/test/ch/ethz/tell/Runner.java
@@ -60,19 +60,22 @@ public class Runner {
         sun.misc.Unsafe unsafe = Unsafe.getUnsafe();
         int cnt = 0;
         while (offset != length) {
+            offset += 8;
             for (Schema.FieldType fieldType : schema.fixedSizeFields()) {
                 switch (fieldType) {
                     case SMALLINT:
+                        //TODO not a 16 bit
+                        System.out.println(fieldType + "->>" + unsafe.getShort(address + offset));
+                        offset += 2;
+                        break;
                     case INT:
                     case FLOAT:
-                        offset += 2;
                         if (unsafe.getInt(address + offset) > 10)
                             System.out.println(fieldType + "->>" + unsafe.getInt(address + offset));
                         offset += 4;
                         break;
                     case DOUBLE:
                     case BIGINT:
-                        offset += 6;
                         long aLong = unsafe.getLong(address + offset);
                         if (9223372032559808513L != aLong)
                             throw new IllegalStateException("Error while reading expected record");
@@ -94,7 +97,8 @@ public class Runner {
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-            offset += 8 - (offset % 8);
+            if (offset %8 != 0)
+                offset += 8 - (offset % 8);
             cnt++;
         }
         System.out.println(String.format("RECORDS FOUND: %d", cnt));
